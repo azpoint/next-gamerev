887e79858dade41ef5fa3a02ad18f31675c10d08
Alejandro Zapata <alejo8708@gmail.com>
2023-11-06 20:04:00 +0100

    app-finished

--- next.config.js
+++ next.config.js
@@ -3,13 +3,16 @@ module.exports = {
 	// output: "export",
 	images: {
 		// unoptimized: true
-		remotePatterns: [
-			{
-				protocol: 'http',
-				hostname: 'localhost',
-				port: '1337',
-				pathname: '/uploads/**'
-			}
-		]
-	}
+		remotePatterns: [toRemotePattern(process.env.CMS_IMAGE_PATTERN)],
+	},
 };
+
+function toRemotePattern(urlString) {
+	const url = new URL(urlString);
+	return {
+		protocol: url.protocol.replace(":", ""),
+		hostname: url.hostname,
+		port: url.port,
+		pathname: url.pathname,
+	};
+}
